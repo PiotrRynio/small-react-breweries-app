40f3c462a53e73d430ac6ee2541c8a7c300c5e22
Piotr Rynio <pwrynio@gmail.com>
2022-07-07 23:17:20 +0200

    feat(breweryView): add brewery view

--- src/views/App/App.test.tsx
+++ src/views/App/App.test.tsx
@@ -1,8 +1,17 @@
+import userEvent from '@testing-library/user-event';
 import { render, screen } from 'test-utils';
+import { getSearchBreweriesWillReturn } from '../../mocks/msw/rest-api/breweriesRequest/mockEndpoints/searchBreweriesMockEndpoints';
+import { breweriesResponse } from '../../mocks/msw/rest-api/breweriesRequest/responses/breweriesResponse';
+import { getBreweryByIdIsLoading } from '../../mocks/msw/rest-api/breweriesRequest/mockEndpoints/breweryByIdMockEndpoints';
+import { getBreweriesWillReturn } from '../../mocks/msw/rest-api/breweriesRequest/mockEndpoints/breweriesMockEndpoints';
 import { App } from './App';
 
 describe(`<App>`, () => {
   it('renders correctly', async () => {
+    // given
+    getBreweriesWillReturn(breweriesResponse);
+    getSearchBreweriesWillReturn(breweriesResponse);
+
     // when
     render(<App />);
 
@@ -10,5 +19,36 @@ describe(`<App>`, () => {
     screen.getByRole('img', { name: /Brewery App Logo/i });
     screen.getByLabelText(/searcher/i);
     screen.getByRole('heading', { name: /Breweries/i, level: 2 });
+    expect(await screen.findByText(/Banjo Brewing/i)).toBeVisible();
+  });
+
+  it(`shows loading status, if you searching`, async () => {
+    // given
+    getBreweryByIdIsLoading();
+    const user = userEvent.setup();
+    render(<App />);
+    const searcher = await screen.findByLabelText(/searcher/i);
+
+    // when
+    await user.type(searcher, 'dog');
+
+    // then
+    expect(searcher).toHaveValue('dog');
+    expect(await screen.findByText(/Loading/i)).toBeVisible();
+  });
+
+  it(`shows items, if you search 'dog' and loading status disappear`, async () => {
+    // given
+    getBreweriesWillReturn(breweriesResponse);
+    getSearchBreweriesWillReturn(breweriesResponse);
+    const user = userEvent.setup();
+    render(<App />);
+    const searcher = await screen.findByLabelText(/searcher/i);
+
+    // when
+    await user.type(searcher, 'dog');
+
+    // then
+    expect(await screen.findByText(/Banjo Brewing/i)).toBeVisible();
   });
 });
